912923a893d5dc0378ecdbde658bc07deb9860fe
TimofeyHub <timatofey@gmail.com>
2023-09-30 17:06:09 +0300

    set base models

--- shop/base/models.py
+++ shop/base/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class Scale(models.Model):
+    value = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.value
+
+
+class ScaleModel(models.Model):
+    name = models.CharField(max_length=255)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    scale = models.ForeignKey(
+        Scale,
+        on_delete=models.PROTECT
+    )
+    description = models.TextField(null=False, blank=True)
+
+    def __str__(self):
+        return self.name
