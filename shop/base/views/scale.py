eafff1c040c74c4de49bc59a124705222081abe8
TimofeyHub <timatofey@gmail.com>
2023-10-24 15:38:45 +0300

    add scale create

--- shop/base/views/scale.py
+++ shop/base/views/scale.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
 from base.models import Scale
 
@@ -10,3 +11,9 @@ class ScaleList(ListView):
         context = super().get_context_data(object_list=None, **kwargs)
         context['page_title'] = 'Scale list'
         return context
+
+
+class ScaleCreate(CreateView):
+    model = Scale
+    fields = '__all__'
+    success_url = reverse_lazy('base:scale-index')
